1ea3627da027d083a62d47a3dcd40085b53d72e8
priest666 <jerry.prestley.com>
2022-10-22 10:57:00 +0300

    firstone commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const navBtn = document.querySelector('.mobile-nav');
+const nalst = document.querySelector('.mobilelst');
+navBtn.addEventListener('click', ()=>{
+    const visibility = nalst.getAttribute('data-visible');
+
+    if (visibility == 'false') {
+        nalst.setAttribute('data-visible', true);
+        navBtn.setAttribute('aria-expanded', true);
+        document.body.classList.add('no-scroll')
+    }else{
+        nalst.setAttribute('data-visible', false);
+        navBtn.setAttribute('aria-expanded', false);
+        document.body.classList.remove('no-scroll')
+    }
+})
+
+const secone = document.querySelector('.title2');
+
+option = {
+    root: null,
+    threshold: 0, //how much of it is visible
+    rootMargin: "-70px" //sets mergin
+};
+
+const observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            console.log(entry.isIntersecting);
+            document.querySelector('.title21').classList.add('slidefromside');
+            observer.unobserve(entry.target);
+        }
+        
+    })
+}, option);
+
+observer.observe(secone);
+
+// position: relative;
+// overflow: hidden;
+// top: -100px;
